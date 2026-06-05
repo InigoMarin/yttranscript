9d558e50e54caf4f7e7637639e1dd13680ca1002
Inigo Marin Garcia <inigomaringarcia@gmail.com>
2026-06-05 22:51:00 +0200

    Bump version to 1.7.1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all pkg install clean help
 
-VERSION := 1.7.0
+VERSION := 1.7.1
 PKGNAME := yttranscript
 TARBALL := $(PKGNAME)-$(VERSION).tar.gz
 SRC_DIR := $(PKGNAME)-$(VERSION)
--- yttranscript.py
+++ yttranscript.py
@@ -5,7 +5,7 @@ Download transcripts (subtitles/captions) from YouTube videos.
 Falls back to Whisper transcription when no subtitles are available.
 """
 
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 
 import argparse
 import os
