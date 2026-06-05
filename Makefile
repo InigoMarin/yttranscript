d774781f7fc5f84c6d44e88a33d948b2efcbf762
Inigo Marin Garcia <inigomaringarcia@gmail.com>
2026-06-05 23:07:02 +0200

    Bump version to 1.8.2

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all pkg install clean help
 
-VERSION := 1.8.1
+VERSION := 1.8.2
 PKGNAME := yttranscript
 TARBALL := $(PKGNAME)-$(VERSION).tar.gz
 SRC_DIR := $(PKGNAME)-$(VERSION)
--- yttranscript.py
+++ yttranscript.py
@@ -5,7 +5,7 @@ Download transcripts (subtitles/captions) from YouTube videos.
 Falls back to Whisper transcription when no subtitles are available.
 """
 
-__version__ = "1.8.1"
+__version__ = "1.8.2"
 
 import argparse
 import os
